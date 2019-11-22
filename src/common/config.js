dde69fcf62feb6d1f6a20b7924a454ee16731141
Emir Tucic <emir.tucic@forient.com>
2019-11-22 09:08:06 +0100

    Add changes

---
+++ src/common/config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    // "google": {
+    //     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDIY50CHwsRb0BH\nHnOdV0rFjvYrySoBzbImG0Q7eHVvM1li9H4+y7T69a8F7SyOzJ//mEuZ5dAjF5LP\nA4xic6/nM8v97uwIa4xHKrYJbhsov3Myy2gO1YTAxUrcF2aiM/Itdkn+nISjk2G8\nPaZLPRmcsRH4Y+xdmpIq2x0sO37cmEIEkunmSZtbCXS/cW2zrrv4e4xTmbptJ5wM\nNMrRFY/AUHzFXBt3H+iNkT6OkNfzpy6E6sdbHP2yaxKf0JVytRjouROhQxXq+Hks\n4ULX3nW0DQcIu7KPQUYXM6E2wPdgDnQ38XpuhcD3nLhGQLIP1NIKeISwERawaU1w\nkXX018NZAgMBAAECggEACEbtDlBf8alpu0ZLuj0Hzp84FdjqTkbDzAUmTVqNZzl0\nwLyellNtt0v8nb8NLxzOow0UWlf+v+RSYRChdNOYeXFPRL7gqvxeP/ZIrJ0KrCU0\nSe3S74HsSNYdYHfq+low76XKKqUmaWdGCzN0sfjlHO7Hcj4WXktrLC/+xXvzooLo\nxXo015RltyC7zHZEd8kOarzTqB/Fzx2uewZCnOqpn04lVsSjT6K0bQP1C9u0rEc6\ndPtFBH1BLpi/pmeupkd/StOirZHIeZtoCVlnPp7RUXRuVgqWcaT3jQ6EyPNcgJf6\neogBM1RiG8iZu7KUm70tWyIJbr31tLC8dicSTyWi7QKBgQDrk55LaK8Bjpmwgc2x\nrONKo9KQJIOdUTObQZaCtrqGzwtcBa/pJ8R6mJcbi6MpFwfMCSWTn9oXop+wlXF4\nsO1VtJ0VxhgWVFjI11oQMkEMXx3e712KD/Zxu2rNPzu+sEiH39+OdJ6b46/aFvCp\nRnYNYorYbjwskyw8UUf5lc+YswKBgQDZwwtJHU8zbLCNwAt750DhCXRFw4UlEEZR\nG+DMY576wpMWQnbyQq7tvzmuhOP8mXzMGRDDZ9M+NvPuFWeeUiydQCBjf8Ukhzhb\nn3CiVNi2YVi3U9GtKA9r21oyTZCfHBq+zs3DL1/GNW4wt0YrjtoT+HbvO738fmoR\nUmYYxMtBwwKBgDNMRtkG560AH2l3KpJ5Ill7iPuIZ/yW1RUG2D6Iid+XMFMOjfrI\nhRlpJuaBprro3/LHPx6BXZ1t9TYfpMDyTgwus1Ite8tI/LhnZJ5IQy/tenS0TPsm\ndLTZae1dyqXAf1rPHkBIFs29a+qiui8yP7y0a6Wrg4D1X1mXHYQ04irPAoGBAM0Q\n08vigfcXX5vdQfNIbtx8nrYjzXp9vPocmrFfDxNtsD3CEWlKSpkw/wzNsDTsBY3d\n/N9HIrLv5P5Lwexhzgm3nciVRf2EKRp/Jv53kC0Ptw/NKyM5GmyjgGjmik6LlNQi\nw7oPHs30+vN9Y+MY/1zXGolnZR4lnZT6V5Lzn8UpAoGANGvvsAX2LOG5Zz2VsGjv\ntBUhkPGei/sVgmZPIZO7aFXfrr1EF2RJnMFHMnyPB7IKgTwcl6emri17I1liUjBy\ncoUH7qvgViSoiZ1AFYx4EQFwbbzpDVLExpug2coDwMXBRWOTd8Hp0UdabuiJJeS7\n+hht73LPtgab9xJSLtNs3pM=\n-----END PRIVATE KEY-----\n",
+    //     "client_email": "google-analytics-api@api-test-project-altchar.iam.gserviceaccount.com",
+    // },
+    // "view_id":200220825,      
+    // "scopes": "https://www.googleapis.com/auth/analytics.readonly",
+    // "capi": {
+    //     "url":"https://capi.beta.altchar.com/v1.0/",
+    //     "key":"NzOPmBslNG92I8eQmvHiG6xS495D2BgG1l7sVRiu"
+    // },
+    // "aws": {
+    //     "region":"eu-west-1",
+    //     "queueUrl":"https://sqs.eu-west-1.amazonaws.com/336777809222/GlideScheduler-AltChar-PROD",
+    //     "tableName":"GlideScheduler-AltChar-PROD",
+    //     "bucketName":"altchar-deliver-assets-prod",
+    //     "handler":"GlideScheduler-AltChar-Handler",
+    //     "scheduler":"GlideScheduler-AltChar-Scheduler"
+    // }
+
+    "google": {
+        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDjIp/OJhJtlC35\n+FKHzziMk0TDugSuGY3RphcXc0BJe4sdBmfObzWwiMVeJZMxdiDM6JSuy2g1yvLt\nseRbf5yO+RyeYITxC5fqJ3H6fwQOYm/I1bNbVARygQztQkP0nEJ8mMt3xz9iqjXd\n9dDHaQJ+HrmkK0b85TvetHLgVFeDpGikfzXrmnY+OaX2DNOxeoYQecQ3NxDkTcST\nWawuVzIm7EXQGPwsBZsQRNZW1lnAChYiRqn6MYYMkvz1k0j5F8S8O40xZtgs/qnh\nhHS9GBDLMU7plNaiAqKau1s/TEoN0yaFEJsATvBMpUCzMXu/8UaP0lIJ/JqsJ5qI\nNa2XSNAHAgMBAAECggEAKRhfJIvT4rqgNO1+dS8Dcr/Efcs+xIdAFxH/dJE4LPNq\n4JYDmyCY7LdaSmXvA1B1r81W9Wc9FBqDqIj6oaltmJqu2GLrOreXxVhcsW9guv+J\nbSNpH5mMcnEx9w15wRyylNuKWMIWkFHujBnJLAMuXcZOXUstPV12Xx1z+W2ZJep2\neFVBG0cC7dMEP9BG++wPxrUc3WQeBbglA40Iazt+E32dDozi5tLTbmPq00V3tHGi\nxw35/LfMQmRaYJYal88e9m2kD7VDVwOvMAn3MNUmmc14ufZ4+bbTx6lBWQxEQcqG\nKaSsmn3cuxdBMkK8ZaOsnT6B46FCWIRkE2AiVH9Q9QKBgQDxcV1LaUCLOV8eIEVm\nfcDhV6dZ2hrtwgpckS80K3TjJCPYPZzqVEmpz2jwgko9wEI3eU35rCr+uTAniFWT\nqoQVwu49Axbj9kvSmZKYhzCl9RGGYTMh58CuvSDxpQsUHuUC0Y7p1/xuKfEL6g+x\nQ2c04/bNN3d8oaU6sdY0C3Zf3QKBgQDw1GyjrDNKIs9/Jh0e2UNnMj6vsvCwl9ka\ns8iPYx8/7kpKAqp/2TrPD7aYzg2jKddHsJvUl8fVJEki2UbAHU325h4ILrcsX5FA\n9/khh1gCVRo78SQ1gZP+XolCYNhgEHxBNfHCRkogcXEhra6gxyFXuIyz5U+Fe3Cg\nKH7k6LKjMwKBgQCYBepZXkdeolwqyuUH2KyG9rsPz03fYKtHh77yGV662nmbYYgY\n38OWIVuQ/GikoxbOeRnI2EgmJRKWAbjohpguX5PEZNqX5O/vBfWRBqZyCHHla/RX\nJ+Zh4AILRQFJxP4rnSlgQjfWYaA+AT2qPqeQXO18X61jkyBHacxW7tS1HQKBgCF7\n5376Okdcf60WB4Z6fryiZ8TWBFEJS85CoOgOyBGDeCDCLiKIeXzAmoq0W4EMGDqH\ne6MbV1VzLhgCTav4Zxak+Bf7JsysnO2DjMf0oW/xlSwwYvBti74aNstb27m/kQcg\nEPUKANRx8WDVXSpNhX1E04tPnrbYathZyQrar7uxAoGBAMQwzAqW+M54RYkygIoS\njP+KFK8bhGkbgL9wJDvmXlluV3vQEPjr780nYOcQuS4n2LAeTRKsehGCr2ZwrYIS\nhZ7GCzYFIl0P+xhnogFSTouBssqt75oOyQkLDJtyqIOtF5htoE7SOAB7rywucsb4\nIvfSRAvq0uMgYuKZSecwNuwz\n-----END PRIVATE KEY-----\n",
+        "client_email": "altchar-glide-deliver@altchar-glide-deliver.iam.gserviceaccount.com",
+    },
+    "view_id":128016313,      
+    "scopes": "https://www.googleapis.com/auth/analytics.readonly",
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,123 @@
 var google = require("googleapis").google;
-var scopes = "https://www.googleapis.com/auth/analytics.readonly";
-var dotenv = require('dotenv');
-dotenv.config();
-var clientEmail = process.env.CLIENT_EMAIL;
-var privateKey = process.env.PRIVATE_KEY;
-var viewID = process.env.VIEW_ID;
-var jwt = new google.auth.JWT(clientEmail, null, privateKey, scopes);
-jwt.authorize(function (err, response) {
-    if (response) {
-        google.analytics("v3").data.ga.get({
-            auth: jwt,
-            ids: "ga:" + viewID,
-            "start-date": "30daysAgo",
-            "end-date": "today",
-            metrics: "ga:pageviews",
-            dimensions: "ga:pagePath",
-            sort: "-ga:pageviews",
-            filters: "ga:pagePath=~^/([\\w\\d-._~]+)/([\\w\\d-._~]+)-([\\w\\d-._~]+){12}$",
-            "start-index": "1",
-            "max-results": "10"
-        }, function (err, result) {
-            if (result) {
-                console.log(result.data.rows);
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+const config = require('./common/config.js');
+
+var clientEmail = config.google.client_email;
+var privateKey = config.google.private_key;
+var viewID = config.view_id; 
+var scopes = config.scopes; 
+
+const authenticateUser = async (clientEmail, privateKey, scopes) => {
+    try{
+        var jwt = await new google.auth.JWT(clientEmail, null, privateKey, scopes);
+        return jwt;    
+    }catch(error){
+        return error;
+    }
+}
+
+const authorizeUser = async (jwt) => {
+    return new Promise(function(resolve, reject) {
+        jwt.authorize(function (err, response) {
+
+            if(err){
+                console.log('Error Authorizing the user: ', err)
+                reject(null);
+            }
+
+            if(response){
+                resolve(response);
             }
         });
+    })
+}
+
+const getDataFromGoogleAPI = async(query = {}) => {
+    return new Promise(function(resolve, reject) {
+        google.analytics("v3").data.ga.get(query, function (err, result) {
+            if (result) {
+                resolve(result.data.rows);
+            }
+            if(err){
+
+                reject(err);
+            }
+        });
+    })
+}
+
+const uploadToS3 = (client, params) => {
+    console.log(`Uploading ${params.fileName} to S3...`);
+    const s3Params = {
+      Bucket: config.aws.bucketName,
+      Key: `${params.env}/feeds/most-popular/${params.fileName}`,
+      Body: params.fileBody,
+      ACL: "public-read",
+      ContentType: "application/json"
+    };
+
+    return new Promise((resolve, reject) => {
+      client.upload(s3Params, function(err, data) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+};
+
+const createQuery = (auth, viewId, startDate = "30daysAgo", endDate = "today", metrics = "ga:pageviews", options)  => {
+    const query = {
+        auth,
+        ids: "ga:" + viewId,
+        "start-date": startDate,
+        "end-date": endDate,
+        metrics,
+        ...options,
+    } 
+    return query;
+}
+
+const createListOfData = (data) => {
+    const dataList = data.map(el => {
+        return  {
+            "article_url": `${el[0]}`,
+            "number_of_views": `${el[1]}`
+        }
+    })
+
+    return dataList;
+}
+
+async function run(params = {env:'qa'}){
+    const jwt = await authenticateUser(clientEmail, privateKey, scopes);
+
+    try{
+        const isAuthorized = await authorizeUser(jwt);
+
+        if(isAuthorized){
+            const query = createQuery(jwt,viewID, "30daysAgo", "today", "ga:pageviews", {dimensions: "ga:pagePath",sort: "-ga:pageviews",filters: "ga:pagePath=~^/([\\w\\d-._~]+)/([\\w\\d-._~]+)-([\\w\\d-._~]+){12}$","start-index": "1","max-results": "10"});
+            const data = await getDataFromGoogleAPI(query);
+            const dataList = createListOfData(data);
+            /*
+            await uploadToS3(s3,{
+                fileName: `feed.json`,
+                fileBody: `${JSON.stringify(dataList)}`,
+                env: params.env,
+            });
+            */
+        }
+            
+    }catch(e){
+        console.log(e)
     }
-});
+}
+
+
+run({env:'dev'});
+module.exports = {
+    run
+};
